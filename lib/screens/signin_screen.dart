7077bc39f1cef903dbfbe88cd387c8c4871ce706
Priyanshu Desai <pdesai1@seattleu.edu>
2025-01-27 14:51:41 -0800

    created new SignIn ,SignUp and home Screens an added code for color gradients

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body:Container(decoration: BoxDecoration(gradient:LinearGradient(colors:))));
+  }
+}
