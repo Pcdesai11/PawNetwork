e59ee3aaf29db749b4853519354401ede88c14ed
Priyanshu Desai <pdesai1@seattleu.edu>
2025-02-06 01:59:24 -0800

    made changes in main.dart to show the bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pawnetwork/screens/community_feed_screen.dart';
 import 'package:pawnetwork/screens/pet_profile_screen.dart';
 import 'screens/signin_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/main_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,12 +42,11 @@ class App extends StatelessWidget {
           );
         }
 
-        // Once complete, show your application
+
         if (snapshot.connectionState == ConnectionState.done) {
           return MyApp();
         }
 
-        // Otherwise, show loading indicator
         return MaterialApp(
           home: Scaffold(
             body: Center(
@@ -96,11 +97,24 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: SignInScreen(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasData) {
+            return MainScreen();
+          }
+
+          return SignInScreen();
+        },
+      ),
       routes: {
-        '/pet-profile': (context) => PetProfileScreen(),
-        '/signup': (context) => SignUpScreen(),
-        '/home': (context) => HomeScreen(),
+        '/main': (context) => MainScreen(),
+        '/signin': (context) => SignInScreen(),
+        '/signup': (context) => PetProfileScreen(),
       },
     );
   }
