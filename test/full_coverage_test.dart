76a8093dec426a134eda6f659521d447aef7f537
Priyanshu Desai <pdesai1@seattleu.edu>
2025-02-24 12:48:59 -0800

    updated some files an made a few new files to support live interaction of community feed

---
+++ test/full_coverage_test.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unused_import
+import 'package:pawnetwork/auth.dart';
+import 'package:pawnetwork/main.dart';
+import 'package:pawnetwork/models/pet.dart';
+import 'package:pawnetwork/models/post.dart';
+import 'package:pawnetwork/screens/community_feed_screen.dart';
+import 'package:pawnetwork/screens/home_screen.dart';
+import 'package:pawnetwork/screens/main_screen.dart';
+import 'package:pawnetwork/screens/pet_profile_screen.dart';
+import 'package:pawnetwork/screens/signin_screen.dart';
+import 'package:pawnetwork/screens/signup_screen.dart';
+
+void main() {}
